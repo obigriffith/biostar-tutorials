0e13426046e9599bbe698d1e61f5019538bfe8fc
Obi Griffith <ogriffit@genome.wustl.edu>
2016-12-18 20:56:32 -0600

    Remove UTF8 encoding of unicode character A0 non-breaking space characters - probably introduced by copy-paste from word, causing troubles to linux users

--- Heatmaps/heatmap.3.R
+++ Heatmaps/heatmap.3.R
@@ -47,7 +47,7 @@ heatmap.3 <- function(x,
                       ColSideColorsSize = 1,
                       RowSideColorsSize = 1,
                       KeyValueName="Value",...){
- 
+
     invalid <- function (x) {
       if (missing(x) || is.null(x) || length(x) == 0)
           return(TRUE)
@@ -57,7 +57,7 @@ heatmap.3 <- function(x,
           return(all(is.na(x)))
       else return(FALSE)
     }
- 
+
     x <- as.matrix(x)
     scale01 <- function(x, low = min(x), high = max(x)) {
         x <- (x - low)/(high - low)
@@ -228,7 +228,7 @@ heatmap.3 <- function(x,
         lwid <- c(keysize, 4)
     if (missing(lmat) || is.null(lmat)) {
         lmat <- rbind(4:3, 2:1)
- 
+
         if (!missing(ColSideColors)) {
            #if (!is.matrix(ColSideColors))
            #stop("'ColSideColors' must be a matrix")
@@ -238,7 +238,7 @@ heatmap.3 <- function(x,
             #lhei <- c(lhei[1], 0.2, lhei[2])
              lhei=c(lhei[1], side.height.fraction*ColSideColorsSize/2, lhei[2])
         }
- 
+
         if (!missing(RowSideColors)) {
             #if (!is.matrix(RowSideColors))
             #stop("'RowSideColors' must be a matrix")
@@ -250,16 +250,16 @@ heatmap.3 <- function(x,
         }
         lmat[is.na(lmat)] <- 0
     }
- 
+
     if (length(lhei) != nrow(lmat))
         stop("lhei must have length = nrow(lmat) = ", nrow(lmat))
     if (length(lwid) != ncol(lmat))
         stop("lwid must have length = ncol(lmat) =", ncol(lmat))
     op <- par(no.readonly = TRUE)
     on.exit(par(op))
- 
+
     layout(lmat, widths = lwid, heights = lhei, respect = FALSE)
- 
+
     if (!missing(RowSideColors)) {
         if (!is.matrix(RowSideColors)){
                 par(mar = c(margins[1], 0, 0, 0.5))
@@ -282,9 +282,9 @@ heatmap.3 <- function(x,
             }
         }
     }
- 
+
     if (!missing(ColSideColors)) {
- 
+
         if (!is.matrix(ColSideColors)){
             par(mar = c(0.5, 0, 0, margins[2]))
             image(cbind(1:nc), col = ColSideColors[colInd], axes = FALSE)
@@ -306,7 +306,7 @@ heatmap.3 <- function(x,
             }
         }
     }
- 
+
     par(mar = c(margins[1], 0, 0, margins[2]))
     x <- t(x)
     cellnote <- t(cellnote)
@@ -403,7 +403,7 @@ heatmap.3 <- function(x,
             min.raw <- min(x, na.rm = TRUE)
             max.raw <- max(x, na.rm = TRUE)
         }
- 
+
         z <- seq(min.raw, max.raw, length = length(col))
         image(z = matrix(z, ncol = 1), col = col, breaks = tmpbreaks,
             xaxt = "n", yaxt = "n")
